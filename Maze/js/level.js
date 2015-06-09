a0f6b1bf327b1042ee2412c0c40b738cf800b362
Ivar Clemens <post@ivarclemens.nl>
2015-06-09 16:17:15 +0100

    Removed queryStr function.

--- Maze/js/level.js
+++ Maze/js/level.js
@@ -1,12 +1,12 @@
 
 function Level()
-{	
+{
 	// Server to load levels from
 	//this.server = "http://www.ivarclemens.nl/platform_game/ldb/";
 
 	this.levelMap = [[3]];
-	
-	if(querySt("player") == 1)
+
+	if(getQueryField("player") == 1)
 	this.levelMap = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 	                 [1,3,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
 	                 [1,0,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1],
@@ -49,8 +49,8 @@ function Level()
 	                 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 	                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
 
-	
-	if(querySt("player") == 2)
+
+	if(getQueryField("player") == 2)
 	this.levelMap = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 	                 [1,3,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
 	                 [1,0,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1],
@@ -92,8 +92,8 @@ function Level()
 	                 [1,0,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1],
 	                 [1,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 	                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
-	
-	if(querySt("player") == 3)
+
+	if(getQueryField("player") == 3)
 		this.levelMap = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 		                 [1,3,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,2,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
 		                 [1,0,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1],
@@ -135,12 +135,12 @@ function Level()
 		                 [1,0,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1],
 		                 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 		                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
-	
+
 	this.setup = function()
-	{			
+	{
 		this.bombImage = new Image();
 		this.bombImage.src = "images/bomb.png";
-		
+
 		if('game' in this) {
 			var bounds = {
 					x: 0,
@@ -149,28 +149,17 @@ function Level()
 					height: (this.getHeight()-1)/2 * widthspace +  (this.getHeight()+1)/2 * widthwall
 				};
 			this.game.setLevelBounds(bounds);
-						
+
 			this.game.setSize(bounds.width, bounds.height);
-		}	
+		}
 	}
-	
-	
+
+
 	this.update = function(input)
 	{
 	}
 
-	/**
-	 * Draws a single sprite in the grid
-	 
-	this.drawSprite = function(context, x, y, sprite)
-	{
-		if(sprite == 1 && this.game && !this.game.editMode)
-			return;
-		
-		return this.game.spriteManager.drawSprite(context, box, sprite, frame);
-	}
-   */
-	
+
 	/**
 	 * Draw entire level
 	 */
@@ -194,13 +183,13 @@ function Level()
 					y = (i-1)*(widthspace+widthwall)/2 + widthwall;
 					h = widthspace;
 				}
-				
-			
+
+
 				if (this.levelMap[i][j] == 1){
 					context.fillStyle="#000000";
 					context.fillRect(x,y,w,h);
 				}
-				
+
 				if (this.levelMap[i][j] == 2){
 					context.drawImage(this.bombImage, x, y, w, h);
 				}
@@ -226,15 +215,15 @@ Level.prototype.getWidth = function()
  * Sets the sprite at a specific block
  */
 Level.prototype.setSprite = function(coords, sprite)
-{  
+{
 	// Check invalid coordinates
 	if(coords.x < 0 || coords.y < 0)
 		return false;
 
 	// Expand level if not big enough
-	if(this.levelMap.length < coords.y || 
+	if(this.levelMap.length < coords.y ||
 	   this.levelMap[0].length < coords.x) {
-		
+
 		// Required dimensions
 		var height = Math.max(1 + coords.y, this.levelMap.length);
 		var width = Math.max(1 + coords.x, this.levelMap[0].length);
@@ -242,7 +231,7 @@ Level.prototype.setSprite = function(coords, sprite)
 		for(var i = 0; i < height; i++) {
 			if(i >= this.levelMap.length)
 				this.levelMap[i] = [];
-			
+
 			for(var j = this.levelMap[i].length; j < width; j++)
 				this.levelMap[i][j] = 0;
 		}
@@ -258,11 +247,11 @@ Level.prototype.setSprite = function(coords, sprite)
 Level.prototype.resetLevel = function(width, height)
 {
 	this.levelMap = [];
-	
+
 	for(var j = 0; j < height; j++) {
 		this.levelMap[j] = [];
-		
-		for(var i = 0; i < width; i++) {		
+
+		for(var i = 0; i < width; i++) {
 			if(i == 0 || j == 0 || i == (width -  1) || j == (height - 1)) {
 				this.levelMap[j][i] = 1;
 			} else {
@@ -270,7 +259,7 @@ Level.prototype.resetLevel = function(width, height)
 			}
 		}
 	}
-	
+
 	this.setup();
 }
 
@@ -296,7 +285,7 @@ Level.prototype.loadLevel = function(name)
 Level.prototype.saveLevel = function(name)
 {
 	jQuery.ajax({
-		url:  this.server + "/set_level.php?name=" + name, 
+		url:  this.server + "/set_level.php?name=" + name,
 		data: JSON.stringify(this.levelMap),
 		contentType: 'text/plain',
 		method: 'POST'
